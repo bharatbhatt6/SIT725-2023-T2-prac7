b5a4316644c6f4b036c3c1959f4ae0a9a42b5080
bharatbhatt6 <s223148345@deakin.edu.au>
2023-07-25 17:05:55 +1000

    Changes 2

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-let express = require('express');
-let app = express();
-let port = process.env.port || 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/', (req,res)=>{
-    res.render('index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/addTwoNumbers',(req,res)=>{
-    //let payLoad = {}
-    let statusCode = 200;
-    let sucessMesssage = 'successful';
-    let number1 = req.query.number1; // 99
-    let number2 = req.query.number2; // 40
-    let result = parseInt(number1) + parseInt(number2); // convert them to int
+app.get('/addTwoNumbers', (req, res) => {
+  const number1 = parseInt(req.query.number1);
+  const number2 = parseInt(req.query.number2);
+  const result = number1 + number2;
 
-    res.json({
-        message:sucessMesssage, 
-        code: statusCode, 
-        data: result
-    });
+  res.json({
+    message: 'successful',
+    code: 200,
+    data: result,
+  });
 });
 
 app.listen(port, () => {
-    console.log('server started');
+  console.log('server started');
 });
