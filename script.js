b9208324c5e2118cda5719a770cd3c1d9e01e620
bharatbhatt6 <s223148345@deakin.edu.au>
2023-08-15 17:33:10 +1000

    test 1

--- script.js
+++ script.js
@@ -1,16 +1,90 @@
-const cardList = [{
-    title: 'cat 2',
-    path: 'Cats/Cat2.jpeg',
-    subTitle: 'About Cat 2',
-    description: 'Description of Cat 2'
-},
-{
-    title: 'cat 3',
-    path: 'Cats/Cat3.jpeg',
-    subTitle: 'About Cat 3',
-    description: 'Description of Cat 3'
-},
-];
+// const cardList = [{
+//     title: 'cat 2',
+//     path: 'Cats/Cat2.jpeg',
+//     subTitle: 'About Cat 2',
+//     description: 'Description of Cat 2'
+// },
+// {
+//     title: 'cat 3',
+//     path: 'Cats/Cat3.jpeg',
+//     subTitle: 'About Cat 3',
+//     description: 'Description of Cat 3'
+// },
+// ];
+
+
+
+// const addCards = (items) => {
+//     items.forEach(item => {
+//         let itemToAppend = `
+//             <div class="col s4 center-align">
+//                 <div class="card medium">
+//                     <div class="card-image waves-effect waves-block waves-light">
+//                         <img class="activator" src="${item.path}">
+//                     </div>
+//                     <div class="card-content">
+//                         <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+//                         <p><a href="#">${item.subTitle}</a></p>
+//                     </div>
+//                     <div class="card-reveal">
+//                         <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+//                         <p class="card-text">${item.description}</p>
+//                     </div>
+//                 </div>
+//             </div>`;
+//         $("#card-section").append(itemToAppend);
+//     });
+// }
+
+// const formSubmitted = () => {
+//     let formData = {};
+//     formData.title = $('#title').val();
+//     formData.path = $('#path').val();
+//     formData.subTitle = $('#subTitle').val();
+//     formData.description = $('#description').val();
+
+//     console.log(formData);
+//     postCat(formData);
+// }
+
+// function postCat(cat) {
+//     $.ajax({
+//         url:'/api/cat',
+//         type:'POST',
+//         data:cat,
+//         success: (result) => {
+//             if (result.statusCode === 201) {
+//                 alert('cat posted');
+//             }
+//         }
+//     });
+// }
+
+// function getAllCats() {
+//     $.get('/api/cats',(result)=>{
+//         if (result.statusCode === 200) {
+//             addCards(result.data);
+//         }
+//     });
+// }
+
+// const cancelform = () => {
+//     $('.modal').modal('close');
+// };
+
+// $(document).ready(function () {
+//     $('.materialboxed').materialbox();
+//     $('#formSubmit').click(() => {
+//         formSubmitted();
+//     });
+//     $('#formcancel').click(() => {
+//         cancelform();
+//     });
+//     // addCards(cardList);
+//     $('.modal').modal();
+//     getAllCats();
+// });
+
 
 const addCards = (items) => {
     items.forEach(item => {
@@ -36,26 +110,47 @@ const addCards = (items) => {
 
 const formSubmitted = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.path = $('#path').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.description = $('#description').val();
 
     console.log(formData);
+    postCat(formData);
 }
 
-const cancelform = () => {
-    $('.modal').modal('close');
-};
+function postCat(cat) {
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('Cat posted');
+                // getAllCats(); // Fetch and update cards after posting
+                // $('#catForm')[0].reset(); // Clear the form
+            }
+        }
+    });
+}
+
+function getAllCats() {
+    $.get('/api/cats',(result)=>{
+        if (result.statusCode === 200) {
+            // $('#card-section').empty(); // Clear existing cards
+            addCards(result.data);
+        }
+    });
+}
 
 $(document).ready(function () {
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(() => {
+    
+    $('#catForm').submit((event) => {
+        event.preventDefault(); // Prevent default form submission behavior
         formSubmitted();
     });
-    $('#formcancel').click(() => {
-        cancelform();
-    });
-    addCards(cardList);
+    
     $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -1,25 +1,194 @@
+// const express = require('express');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+
+// app.use(express.static(__dirname + '/'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+// let collection;
+// const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     collection = client.db().collection('Cats');
+//     console.log(collection);
+//   } catch(ex) {
+//     console.error(ex);
+//   }
+// }
+
+// app.post('/api/cat', function(req,res){
+//   // ask DB to post this CAT
+//   let cat = req.body;
+//   insertCat(cat, (err,result) => {
+//       if (!err) {
+//           res.json({statusCode:201,data:result,message:'success'});
+//       }
+//   });
+// });
+
+// function insertCat(cat, callback) {
+//   collection.insertOne(cat,callback);
+// }
+
+// app.get('/api/cats', (req,res)=>{
+//   getAllCats((err,result)=>{
+//       if (!err) {
+//           res.json({statusCode:200,data:result,message:'success'});
+//       }
+//   });
+// });
+
+// function getAllCats(callback) {
+//   collection.find({}).toArray(callback);
+// }
+
+
+// app.listen(port, () => {
+//   console.log('server started');
+//   run().catch(console.dir);
+// });
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(__dirname + '/'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// let collection;
+// const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     collection = client.db().collection('Cats');
+//     console.log("Connected to database");
+//   } catch(ex) {
+//     console.error("Error connecting to database:", ex);
+//   }
+// }
+
+// app.get('/api/cats', async (req, res) => {
+//   try {
+//     const cats = await getAllCats();
+//     res.json({ statusCode: 200, data: cats, message: 'success' });
+//   } catch (err) {
+//     console.error("Error fetching cats:", err);
+//     res.status(500).json({ statusCode: 500, message: 'Internal Server Error' });
+//   }
+// });
+
+// async function getAllCats() {
+//   return collection.find({}).toArray();
+// }
+
+// app.listen(port, () => {
+//   console.log('Server started');
+//   run().catch(console.dir);
+// });
+
+
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let collection;
+const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/addTwoNumbers', (req, res) => {
-  const number1 = parseInt(req.query.number1);
-  const number2 = parseInt(req.query.number2);
-  const result = number1 + number2;
+async function run() {
+  try {
+    await client.connect();
+    collection = client.db().collection('Cats');
+    console.log("Connected to database");
+  } catch(ex) {
+    console.error("Error connecting to database:", ex);
+  }
+}
 
-  res.json({
-    message: 'successful',
-    code: 200,
-    data: result,
+
+app.post('/api/cat', function (req, res) {
+  let cat = req.body;
+  insertCat(cat, (err, result) => {
+      if (!err) {
+          res.json({ statusCode: 201, data: result, message: 'success' });
+      }
   });
 });
 
+app.get('/api/cats', (req, res) => {
+  getAllCats((err, result) => {
+      if (!err) {
+          res.json({ statusCode: 200, data: result, message: 'success' });
+      }
+  });
+});
+
+function insertCat(cat, callback) {
+  collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback) {
+  collection.find({}).toArray(callback);
+}
+
+
+
 app.listen(port, () => {
-  console.log('server started');
+  console.log('Server started');
+  run().catch(console.dir);
 });
