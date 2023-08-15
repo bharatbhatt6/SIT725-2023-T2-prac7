fa1baa911cd7f8f06ca801c236e89c63d45d3b8a
bharatbhatt6 <s223148345@deakin.edu.au>
2023-08-15 18:29:44 +1000

    FINAL

--- server.js
+++ server.js
@@ -1,143 +1,13 @@
-// const express = require('express');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-
-// app.use(express.static(__dirname + '/'));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
-
-// let collection;
-// const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-//     collection = client.db().collection('Cats');
-//     console.log(collection);
-//   } catch(ex) {
-//     console.error(ex);
-//   }
-// }
-
-// app.post('/api/cat', function(req,res){
-//   // ask DB to post this CAT
-//   let cat = req.body;
-//   insertCat(cat, (err,result) => {
-//       if (!err) {
-//           res.json({statusCode:201,data:result,message:'success'});
-//       }
-//   });
-// });
-
-// function insertCat(cat, callback) {
-//   collection.insertOne(cat,callback);
-// }
-
-// app.get('/api/cats', (req,res)=>{
-//   getAllCats((err,result)=>{
-//       if (!err) {
-//           res.json({statusCode:200,data:result,message:'success'});
-//       }
-//   });
-// });
-
-// function getAllCats(callback) {
-//   collection.find({}).toArray(callback);
-// }
-
-
-// app.listen(port, () => {
-//   console.log('server started');
-//   run().catch(console.dir);
-// });
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(express.static(__dirname + '/'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// let collection;
-// const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-//     collection = client.db().collection('Cats');
-//     console.log("Connected to database");
-//   } catch(ex) {
-//     console.error("Error connecting to database:", ex);
-//   }
-// }
-
-// app.get('/api/cats', async (req, res) => {
-//   try {
-//     const cats = await getAllCats();
-//     res.json({ statusCode: 200, data: cats, message: 'success' });
-//   } catch (err) {
-//     console.error("Error fetching cats:", err);
-//     res.status(500).json({ statusCode: 500, message: 'Internal Server Error' });
-//   }
-// });
-
-// async function getAllCats() {
-//   return collection.find({}).toArray();
-// }
-
-// app.listen(port, () => {
-//   console.log('Server started');
-//   run().catch(console.dir);
-// });
-
-
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let collection;
 const uri = "mongodb+srv://bharatbhatt:bharatbhatt1!@cluster0.0aci8bc.mongodb.net/?retryWrites=true&w=majority";
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -146,49 +16,54 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+let collection;
 
-async function run() {
-  try {
-    await client.connect();
-    collection = client.db().collection('Cats');
-    console.log("Connected to database");
-  } catch(ex) {
-    console.error("Error connecting to database:", ex);
-  }
+async function runDB() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+
+        collection = client.db().collection('Cats');
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (ex) {
+        console.error(ex);
+    }
 }
 
-
-app.post('/api/cat', function (req, res) {
-  let cat = req.body;
-  insertCat(cat, (err, result) => {
-      if (!err) {
-          res.json({ statusCode: 201, data: result, message: 'success' });
-      }
-  });
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api/cats', (req, res) => {
-  getAllCats((err, result) => {
-      if (!err) {
-          res.json({ statusCode: 200, data: result, message: 'success' });
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        console.log(result);
+        if(!err){
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+app.post('/api/cat', (req,res)=>{
+  let cat = req.body;
+  console.log(cat);
+  insertCat(cat, (err,result) => {
+      if(!err){
+          res.json({statusCode:201,data:result,message:'success'});
       }
   });
 });
 
 function insertCat(cat, callback) {
-  collection.insertOne(cat, callback);
+    collection.insertOne(cat, callback);
 }
 
 function getAllCats(callback) {
-  collection.find({}).toArray(callback);
+    collection.find({}).toArray(callback);
 }
 
+runDB().catch(console.dir);
 
-
-app.listen(port, () => {
-  console.log('Server started');
-  run().catch(console.dir);
+app.listen(port, async () => {
+    console.log(`Server started on port ${port}`);
+    await runDB();
 });
